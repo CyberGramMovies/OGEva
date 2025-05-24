41fbaeff9aff26349a9f31d959f0cb1aa44e1514
𝗗𝗿 𝗕𝗲𝗻𝗷𝗮𝗺𝗶𝗻 𝗟𝗼𝘂𝗶𝘀 <157006183+rolexpc@users.noreply.github.com>
2025-05-25 00:07:24 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -42,12 +42,7 @@ async def start(client, message):
             parse_mode=enums.ParseMode.HTML
         )
         return
-    kk, file_id = message.command[1].split("_", 1) if "_" in message.command[1] else (False, False)
-    pre = ('checksubp' if kk == 'filep' else 'checksub') if kk else False
     
-    status = await ForceSub(client, message, file_id=file_id, mode=pre)
-    if not status:
-          return
     if len(message.command) == 2 and message.command[1] in ["subscribe", "error", "okay", "help"]:
         buttons = [[            
             InlineKeyboardButton('🆘 Help ', callback_data='help'),
